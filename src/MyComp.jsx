3202d698da63da267e1c0c705ca57c7f1eae64d5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-03-10 16:54:23 +0900

    add

--- src/MyComp.jsx
+++ src/MyComp.jsx
@@ -10,10 +10,25 @@
 
 // export 키워드를 붙여야 외부 파일의 컴포넌트가 import 할 수 있음
 export function MyComp() {
+  const colors = ["black", "white", "blue"];
+  const [first, second] = colors; // 배열의 구조분해할당
+  const user = {
+    name: "Steve",
+    age: 25,
+  };
+  const { name, age } = user; // 객체의 구조분해할당
+  console.log(first + " " + second);
+  console.log(name + " " + age);
+
+  function method1(x) {
+    console.log("method1함수의 출력 : " + x);
+  }
+
   return (
     <>
       <p>나의 컴포넌트</p>
       <MyCompOnly />
+      <div onClick={() => method1("1000")}>여기를 클릭하시오!</div>
     </>
   );
 }
