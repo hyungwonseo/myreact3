a2c9cbbc3639f91f5a50aa5218c5b9aa9e86ae0d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-03-14 12:16:27 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { MyComp } from "./MyComp";
-import { CounterWithoutState } from "./Components/State/CounterWithoutState";
-import { CounterWithState } from "./Components/State/CounterWithState";
-import { NameForm } from "./Components/State/NameForm";
+import { createGlobalStyle } from "styled-components";
+import Menu from "./Components/3_Props/Menu";
+import BasicRouter from "./Components/4_Router/BasicRouter";
+import ReactRouter from "./Components/4_Router/ReactRouter";
+import SendAPI from "./Components/2_Effect/SendAPI";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Poppins;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
 function App() {
   return (
     <div>
-      <NameForm />
+      <GlobalStyle />
+      <SendAPI />
     </div>
   );
 }
