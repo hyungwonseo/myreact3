590233c00bcabd16048a1f1e55ed04492ae1ad53
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-03-17 10:38:29 +0900

    add

--- src/Components/MovieList/MovieListMain.jsx
+++ src/Components/MovieList/MovieListMain.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import MovieWrapper from "./MovieWrapper";
 import MovieList from "./MovieList";
@@ -7,6 +7,14 @@ import MovieDetail from "./MovieDetail";
 import Search from "./Search";
 import MyPage from "./MyPage";
 import Login from "./Login";
+import Navbar from "./Navbar";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Section = styled.div``;
+const Menu = styled.div``;
+const ContentBox = styled.div``;
+const Footer = styled.div``;
 
 function MovieListMain() {
   return (
@@ -19,15 +27,15 @@ function MovieListMain() {
             </Menu>
             <ContentBox>
               <Routes>
-                <Route path="/" element={Home} />
-                <Route path="/movie" element={MovieWrapper}>
-                  <Route index element={MovieList} />
-                  <Route path=":id" element={MovieDetail} />
+                <Route path="/" element={<Home />} />
+                <Route path="/movie" element={<MovieWrapper />}>
+                  <Route index element={<MovieList />} />
+                  <Route path=":id" element={<MovieDetail />} />
                 </Route>
-                <Route path="/search" element={Search} />
-                <Route path="/mypage" element={MyPage} />
-                <Route path="/login" element={Login} />
-                <Route path="*" element={Error} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<Error />} />
               </Routes>
             </ContentBox>
           </Section>
--- src/Components/MovieList/MovieWrapper.jsx
+++ src/Components/MovieList/MovieWrapper.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function MovieWrapper() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>MovieWrapper입니다.</h1>
+    </div>
+  );
 }
 
 export default MovieWrapper;
