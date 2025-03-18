56fabac1c8a9ef2d61e4cf0a85a2cb31b8590a4d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-03-18 09:25:41 +0900

    Update MovieList.jsx

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function MovieList() {
   return (
     <div>
-      <h1>MovieList입니다.</h1>
+      <h1>MovieList</h1>
+      <Tab>버튼</Tab>
+      <Container>
+        <Card>
+          <Img></Img>
+          <Text>타이틀 : </Text>
+          <Text>장르 : </Text>
+          <hr />
+          <Text></Text>
+        </Card>
+      </Container>
     </div>
   );
 }
