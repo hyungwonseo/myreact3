e47655486b33b587e59cb114bfb439dd64d6fb7a
hwseo12 <59567133+hwseo12@users.noreply.github.com>
2025-04-12 01:25:33 +0900

    파일업로드 기능 추가
    로그인 + JWT 토큰

---
+++ src/Components/FileUpload/FileUpload.jsx
@@ -0,0 +1,123 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import Login, { useUserStore } from "./Login";
+
+const Container = styled.div`
+  width: 60%;
+  margin: 10px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  gap: 10px;
+`;
+const Input = styled.input`
+  width: 50%;
+  height: 40px;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 5px;
+  outline: none;
+`;
+const Box = styled.div`
+  width: 50%;
+  min-height: 100px;
+  border: 1px solid black;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  gap: 10px;
+`;
+const Button = styled.button`
+  width: 50%;
+  height: 40px;
+  color: white;
+  background-color: dodgerblue;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+function FileUpload() {
+  const { user, isLoggedIn, login, logout } = useUserStore();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedFiles, setSelectedFiles] = useState(null);
+  const [imgUrl, setImgUrl] = useState([]);
+
+  function handleChange(event) {
+    console.log(event.target.files);
+    setSelectedFiles(event.target.files);
+  }
+
+  async function handleUploadButton() {
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    if (selectedFiles) {
+      Array.from(selectedFiles).map((f) => formData.append("files", f));
+    }
+    try {
+      await axios.post("/api/upload", formData, {
+        headers: {
+          Authorization: "Bearer " + user.token,
+        },
+      });
+
+      for (const f of selectedFiles) {
+        const response = await axios.get("/api/download/" + f.name, {
+          responseType: "blob",
+          headers: {
+            Authorization: "Bearer " + user.token,
+          },
+        });
+        const url = URL.createObjectURL(response.data);
+        setImgUrl((prev) => [...prev, url]);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Container>
+          <h1>File Upload</h1>
+          <p>이미지를 선택하여 서버로 업로드합니다.</p>
+          <Input
+            type="text"
+            placeholder="제목"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="내용을 적어주세요"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <input multiple type="file" onChange={handleChange} />
+          <Box>
+            {selectedFiles
+              ? Array.from(selectedFiles).map((file, i) => (
+                  <div key={i}>
+                    <div style={{ color: "dodgerblue" }}>{file.name}</div>
+                    <img src={imgUrl[i]} />
+                  </div>
+                ))
+              : null}
+          </Box>
+          <Button onClick={handleUploadButton}>Upload</Button>
+        </Container>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
+
+export default FileUpload;
---
+++ src/Components/FileUpload/Login.jsx
@@ -0,0 +1,171 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+import { IconEmail, IconPassword } from "../MovieList/Icons";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100%;
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: hsl(218deg 50% 91%);
+`;
+const Box = styled.div`
+  width: 300px;
+  height: auto;
+  background-color: hsl(213deg 85% 97%);
+  padding: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  border-radius: 30px;
+  box-shadow: 0 0 2em hsl(231deg 62% 94%);
+`;
+const Smallbox = styled.div`
+  width: 100%;
+  background: hsl(0deg 0% 100%);
+  box-shadow: 0 0 2em hsl(231deg 62% 94%);
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  border-radius: 20px;
+  color: hsl(0deg 0% 30%);
+`;
+const Icon = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+const Input = styled.input`
+  outline: none;
+  border: none;
+  &::placeholder {
+    color: hsl(0deg 0% 0%);
+    font-size: 0.9em;
+  }
+`;
+const Button = styled.button`
+  width: 100%;
+  padding: 1em;
+  margin-top: 10px;
+  background-color: hsl(233deg 36% 38%);
+  color: hsl(0 0 100);
+  border: none;
+  border-radius: 30px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  &:hover {
+    transform: translateY(-2px);
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+
+// 1. Zustand를 이용한 글로벌 상태관리 기본 사용법
+// export const useUserStore = create((set) => ({
+//   user: null,
+//   login: (email) => set({ user: { email } }), // 로그인 처리
+//   logout: () => set({ user: null }), // 로그아웃 처리
+// }));
+
+// 2. Zustand로 데이터를 스토리지에 저장하는 옵션 사용법(세션/로컬 스토리지에 자동으로 저장)
+export const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      authority: null,
+      isLoggedIn: false,
+      login: (user, token, authority) =>
+        set({ user, token, authority, isLoggedIn: true }), // 로그인 처리
+      logout: () =>
+        set({ user: null, token: null, authority: null, isLoggedIn: false }), // 로그아웃 처리
+    }),
+    {
+      name: "user-storage", // sessionStorage에 저장될 키 이름
+      storage: createJSONStorage(() => sessionStorage), // sessionStorage에 저장
+    }
+  )
+);
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const buttonRef = useRef(null);
+  const { user, login, logout } = useUserStore();
+
+  async function handleSubmit() {
+    try {
+      const response = await axios.post("/api/authenticate", {
+        username: username,
+        password: password,
+      });
+      console.log(response);
+      login({
+        user: response.data.username,
+        token: response.data.token,
+        authority: response.data.authority,
+      });
+      setUsername("");
+      setPassword("");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      buttonRef.current.click();
+    }
+  }
+
+  return (
+    <div>
+      <Container>
+        <Box>
+          <h1>LOGIN</h1>
+          <br />
+          <Smallbox>
+            <label>Username</label>
+            <Icon>
+              <IconEmail />
+              <Input
+                type="text"
+                value={username}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+            </Icon>
+          </Smallbox>
+          <Smallbox>
+            <label>Password</label>
+            <Icon>
+              <IconPassword />
+              <Input
+                type="password"
+                autoComplete="off"
+                value={password}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="· · · · · · · · · · · ·"
+              />
+            </Icon>
+          </Smallbox>
+          <Button ref={buttonRef} onClick={handleSubmit}>
+            로그인
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+}
+
+export default Login;
